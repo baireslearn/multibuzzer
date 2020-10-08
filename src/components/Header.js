8fe620df5270c8f5597862e3805a1928426d413c
Baires Learn <37806108+baireslearn@users.noreply.github.com>
2020-10-08 15:41:27 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,7 +46,7 @@ export default function Header({
     <header>
       <Navbar>
         <Navbar.Brand>
-          <Logo /> Multibuzzer
+          <Logo /> e-ABC - ¡Canté pri!
         </Navbar.Brand>
         <div className="nav-buttons">
           {!isNil(sound) ? (
